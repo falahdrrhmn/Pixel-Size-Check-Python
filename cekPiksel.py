eb3addaa21c924b3a8ce35cb7a9d435fb9e8e285
Muhammad Falah A <92344349+falahdrrhmn@users.noreply.github.com>
2023-12-08 13:10:36 +0700

    Update cekPiksel.py

--- cekPiksel.py
+++ cekPiksel.py
@@ -6,7 +6,15 @@ def get_image_size(image_path):
         raise ValueError(f"Gak ada image di {image_path}")
     return image.shape[1], image.shape[0]
 
-image_path = "contoh_citra.jpg"
-width, height = get_image_size(image_path)
-print(f"Lebar citra: {width} piksel")
-print(f"Tinggi citra: {height} piksel")
+image_path_test = "Test\Melanoma\AUG_0_156.jpeg"
+width_test, height_test = get_image_size(image_path_test)
+
+image_path_train = "Train\Melanoma\AUG_0_11.jpeg"
+width_train, height_train = get_image_size(image_path_train)
+
+
+print(f"Lebar Citra Test: {width_test} piksel")
+print(f"Tinggi Citra Test: {height_test} piksel")
+
+print(f"Lebar Citra Train: {width_train} piksel")
+print(f"Tinggi Citra Train: {height_train} piksel")
